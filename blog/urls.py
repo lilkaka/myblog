08b5e7169707ed1b9c35460197dab9eb86f28833
Yang Ruijie <447029019@qq.com>
2018-04-10 15:21:35 +0800

    from lilkaka

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('article/<int:article_id>/',views.article_page),
+    path('edit/<int:article_id>/',views.edit_page),
+    path('edit/action/',views.edit_action),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from . import models
 def index(request):
-    return HttpResponse('Hello World!')
+    articles = models.Article.objects.all()
+    return render(request,'index.html',{'hello':'world','articles':articles})
 # Create your views here.
+
+def article_page(request,article_id):
+    article = models.Article.objects.get(pk = article_id)
+    return render(request,'article_page.html',{'article':article})
+
+def edit_page(request,article_id):
+    if(str(article_id) == '0'):
+        return render(request,'edit_page.html')
+    article = models.Article.objects.get(pk=article_id)
+    return render(request,'edit_page.html',{'article':article})
+
+def edit_action(request):
+    title = request.POST.get('title','TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    article_id = request.POST.get('article_id','0')
+    if article_id == '0':
+        models.Article.objects.create(title=title,content=content)
+        articles = models.Article.objects.all()
+        return render(request,'index.html',{'articles':articles})
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        article.title = title
+        article.content = content
+        article.save()
+        return render(request, 'article_page.html', {'article': article})
+
+
